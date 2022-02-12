93eea54f7c3db0707b6732562df6969faf34d789
Swarnab Garang <swarnabgarang@gmail.com>
2022-02-12 19:02:12 +0530

    added logic to program functions

--- programs/spl-token/src/lib.rs
+++ programs/spl-token/src/lib.rs
@@ -8,19 +8,19 @@ pub mod spl_token {
     use super::*;
 
     pub fn proxy_transfer(ctx: Context<ProxyTransfer>, amount: u64) -> ProgramResult {
-        Ok(())
+        token::transfer(ctx.accounts.into(), amount)
     }
 
     pub fn proxy_mint_to(ctx: Context<ProxyMintTo>, amount: u64) -> ProgramResult {
-        Ok(())
+        token::mint_to(ctx.accounts.into(), amount)
     }
 
     pub fn proxy_burn(ctx: Context<ProxyBurn>, amount: u64) -> ProgramResult {
-        Ok(())
+        token::burn(ctx.accounts.into, amount)
     }
 
     pub fn proxy_set_authority(ctx: Context<ProxySetAuthority>, authority_type: AuthorityType, new_authority: Option<Pubkey>) -> ProgramResult {
-
+        token::set_authority(ctx.accounts.into(), authority_type.into(), new_authority)
     }
 
 }
